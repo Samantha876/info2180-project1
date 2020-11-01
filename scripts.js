2052fd4837062236b1031cc0c6e8f6464f4b4ec2
Samantha Chin <chinsamanthaashley@gmail.com>
2020-11-01 18:59:52 -0500

    Js updated

--- scripts.js
+++ scripts.js
@@ -4,11 +4,11 @@ window.onload=function(){
     button.addEventListener("click",(e)=>{
     e.preventDefault();
     let msg=document.getElementsByClassName("message")[0];
+    let email= document.getElementById('email').value;
 
-    if (email.value===''){
+    if (email===''){
         msg.innerHTML=('Please enter a valid email address');
     }else{
-        let email= document.getElementById('email').value;
         msg.innerHTML= `Thank you! Your email address ${email} has been added to our mailing list.`;
         console.log("Form has been submitted");
     }    
