195173559a21f21900e2f4a02cecf67b0191cd10
Samantha Chin <chinsamanthaashley@gmail.com>
2020-11-01 00:02:53 -0500

    Landing page Updated

--- scripts.js
+++ scripts.js
@@ -1,1 +1,12 @@
-/* Add your JavaScript to this file */
+const form=document.getElementsByClassName("form-group");
+
+form.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    let email= document.getElementbyTagName("input")[0].getAttribute("type");
+    if (email.value==='' || name.value=== null){
+        document.getElementsByClassName("message").innerHTML=('Please enter a valid email address');
+    }else{
+        document.getElementsByClassName("message").innerHTML= `Thank you! Your email address ${email} has been added to our mailing list.`;
+        console.log("Form has been submitted");
+    }    
+})
